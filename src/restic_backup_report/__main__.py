15641a831e9ae6df200ad952bf181fb43636311c
nodedev7425 <buelenard@tutanota.com>
2026-09-12 21:07:18 +0200

    Format register created

--- src/restic_backup_report/__main__.py
+++ src/restic_backup_report/__main__.py
@@ -1,14 +1,22 @@
 import os
 
 from .targets.base_target import TargetTypeRegister
+from .formats.base_format import FormatRegister
+
 from .cli import register_cmds
 
 from .targets.file_target import FileTarget
+
+from .formats.plaintext_format import PlaintextFormat
 
 from .utils.console import print_warn
 
 
 if __name__ == "__main__":
+
+    FormatRegister.register_formats({
+        "plaintext": PlaintextFormat
+    })
 
     TargetTypeRegister.register_targets({
         "file": FileTarget
--- src/restic_backup_report/formats/base_format.py
+++ src/restic_backup_report/formats/base_format.py
@@ -4,3 +4,57 @@ from abc import ABC
 class Format(ABC):
 
     pass
+
+
+class FormatRegister():
+
+
+    _formats: dict[str, type[Format]] = {}
+    
+    
+    @classmethod
+    def register(cls, name: str, format: type[Format]) -> None:
+        if name in cls._formats:
+            raise ValueError(f"Format '{name}' is already registered")
+
+        if format in cls._formats.values():
+            raise ValueError(
+                f"Format '{format.__name__}' is already registered"
+            )
+
+        cls._formats[name] = format
+
+
+    @classmethod
+    def register_formats(cls, formats: dict[str, type[Format]]):
+        for key in formats.keys():
+            cls.register(key, formats[key])
+
+
+    @classmethod
+    def get(cls, name: str) -> type[Format]:
+        try:
+            return cls._formats[name]
+        except KeyError:
+            raise KeyError(f"Format '{name}' is not registered")
+
+
+    @classmethod
+    def get_name(cls, format: type[Format]) -> str:
+        for name, registered_target in cls._formats.items():
+            if registered_target is format:
+                return name
+
+        raise KeyError(
+            f"Format '{format.__name__}' is not registered"
+        )
+
+
+    @classmethod
+    def names(cls) -> list[str]:
+        return list(cls._formats)
+
+
+    @classmethod
+    def has(cls, name: str) -> bool:
+        return name in cls._formats
