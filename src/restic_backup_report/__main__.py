75a11836b8789406d08339376f5c1c0ad303ae9a
nodedev7425 <buelenard@tutanota.com>
2026-09-12 22:50:05 +0200

    Integrate Restic version check

--- src/restic_backup_report/__main__.py
+++ src/restic_backup_report/__main__.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+from .app_info import RESTIC_CLI_MIN_VERSION
+
+from .utils.restic import ResticManager
 
 from .targets.base_target import TargetTypeRegister
 from .formats.base_format import FormatRegister
@@ -9,10 +14,16 @@ from .targets.file_target import FileTarget
 
 from .formats.plaintext_format import PlaintextFormat
 
-from .utils.console import print_warn
+from .utils.console import print_error
 
 
-if __name__ == "__main__":
+def main() -> int:
+    if not ResticManager.is_cli_compatible():
+        print_error(
+            f"This application requires Restic "
+            f"{RESTIC_CLI_MIN_VERSION} or newer."
+        )
+        return 1
 
     FormatRegister.register_formats({
         "plaintext": PlaintextFormat
@@ -23,3 +34,9 @@ if __name__ == "__main__":
     })
 
     register_cmds()
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- src/restic_backup_report/utils/restic.py
+++ src/restic_backup_report/utils/restic.py
@@ -1,6 +1,52 @@
+import subprocess
+import shutil
+
+from packaging.version import Version
+
+from restic_backup_report.app_info import RESTIC_CLI_MIN_VERSION
+
+
+class ResticError(Exception):
+    pass
 
 
 class Repository:
 
-    def __init__(self):
+
+    def __init__(self, name: str, path: str, password: str):
+        self.name = name
+        self.path = path
+        self.password = password
+
+
+class ResticManager:
+
+
+    @staticmethod
+    def get_cli_version() -> str:
+        if shutil.which("restic") is None:
+            raise ResticError("Restic not found")
+
+        result = subprocess.run(
+            ["restic", "version"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        return result.stdout.split()[1]
+
+
+    @staticmethod
+    def is_cli_compatible() -> bool:
+        try:
+            cli_version = Version(ResticManager.get_cli_version())
+        except ResticError:
+            return False
+
+        return cli_version >= Version(RESTIC_CLI_MIN_VERSION)
+
+
+    @staticmethod
+    def is_repo_compatible(repo: Repository) -> bool:
         pass
