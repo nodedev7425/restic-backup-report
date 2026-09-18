7f17e87248b01ff4ecaefa7888dd63303758b9ff
nodedev7425 <buelenard@tutanota.com>
2026-09-18 18:39:32 +0200

    Fix: Added password nonce to repository template

--- src/restic_backup_report/templates/config/repository_template.py
+++ src/restic_backup_report/templates/config/repository_template.py
@@ -5,6 +5,7 @@ template: Template = Template("""
 name: {{ repository_name }}
 path: {{ repository_path }}
 password: {{ repository_password }}
+password_nonce: {{ repository_password_nonce }}
 report: {{ report_interval }}
 backup:
   frequency: {{ backup_frequency }}
